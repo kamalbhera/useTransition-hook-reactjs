f6cec0aa641fbf6c8232c3967db318fc246151c2
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-14 19:29:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useTransition, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductList from './ProductList';
+import { generateProducts } from './data';
+
+const dummyProducts = generateProducts();
+function filterProducts(filterTerm) {
+  if (!filterTerm) {
+    return dummyProducts;
+  }
+  return dummyProducts.filter((product) => product.includes(filterTerm));
+}
 
 function App() {
+  const [isPending, startTransaction] = useTransition();
+  const [filterTerm, setFilterTerm] = useState('');
+
+  const filteredProducts = filterProducts(filterTerm);
+
+  function updateFilterHandler(event) {
+    startTransaction(() => {
+      setFilterTerm(event.target.value);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +30,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>
+            Search Name:
+            <input type="text" onChange={updateFilterHandler} />
+          </label>
+          {isPending && <p style={{ color: 'white' }}>Updating list ...</p>}
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <ProductList products={filteredProducts} />
+          </Suspense>
+        </form>
       </header>
     </div>
   );
